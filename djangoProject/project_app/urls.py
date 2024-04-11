0ed4aa1f8f2922f6b0c163fbf71b848902b3b360
jrdue <jackrdue@uwm.edu>
2024-04-11 16:14:27 -0500

    added login.html and updated views.py

--- djangoProject/project_app/urls.py
+++ djangoProject/project_app/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    #path('login/', views.login_user, name='login'),
+    #path('logout/', views.logout_user, name='logout')
+
 ]
--- djangoProject/project_app/views.py
+++ djangoProject/project_app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+def home(request) :
+    if request.method == 'POST' :
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You are now logged in")
+            return redirect('login')
+        else:
+            messages.success(request, "There was an error logging in, please try again")
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+
+def logout_user(request):
+    pass
