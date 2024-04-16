71d87ea9ca1cc61d0e18a0ca62af957b705f6ce5
jrdue <jackrdue@uwm.edu>
2024-04-16 12:11:46 -0500

    added Classes folder

--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from project_app.views import Login
+import views
+from views import Login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
